be1edc6e4f113a292afb4966da45e381fa8c0206
Mirja Imran <mirjaimran96@gmail.com>
2025-02-05 15:08:23 +0530

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Catbot',
+    version= '0.0.0',
+    author= 'Mirja Imran Hossain',
+    author_email= 'mirjaimran96@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
